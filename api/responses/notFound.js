bf08795260abdb8e8bbce289e5c5c5dfce695e85
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-05-13 23:25:19 +0800

    Lint

--- api/responses/notFound.js
+++ api/responses/notFound.js
@@ -61,7 +61,7 @@ module.exports = function (data) {
             } else {
                 sails.log.warn('res.notFound() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
             }
-            data.code =  statusCode;
+            data.code = statusCode;
             data.status = statusTitle;
             return res.jsonx(data);
         }
