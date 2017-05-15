49302efeb3a7cc56fa4389661fabe37637b92d5c
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-05-15 15:41:43 +0800

    update routes and placeholders

--- config/routes.js
+++ config/routes.js
@@ -5,11 +5,19 @@ module.exports.routes = {
     // Event (To do: policy)
     'post /event/create': 'EventController.create',
     'get  /event/info/:id': 'EventController.getInfo',
-    'post  /event/managers/add': 'EventController.addManagers',
-    'post  /event/managers/remove': 'EventController.removeManagers',
-    'post  /event/racers/add': 'EventController.addRacers',
-    'post  /event/racers/remove': 'EventController.removeRacers',
-    'post  /event/update': 'EventController.update',
+    'post /event/managers/add': 'EventController.addManagers',
+    'post /event/managers/remove': 'EventController.removeManagers',
+    'post /event/update': 'EventController.update',
+
+    // Group
+//    'post /group/create': 'GroupController.create',
+//    'get  /group/info/:id': 'GroupController.getInfo',
+//    'post /group/regs/add': 'GroupController.addRegistrations',
+//    'post /group/regs/remove': 'GroupController.removeRegistrations',
+//    'post /group/races/add': 'GroupController.addRaces',
+//    'post /group/races/remove': 'GroupController.removeRaces',
+//    'post /group/update': 'GroupController.update',
+
     // Manager
     'post /manager/activate': 'ManagerController.activate',
     'post /manager/create': 'ManagerController.create',
@@ -21,6 +29,14 @@ module.exports.routes = {
     'post /manager/update': 'ManagerController.update',
     'post /manager/updatePassword': 'ManagerController.updatePassword',
     // Reset password
+
+    // Race
+//    'post /race/create': 'RaceController.create',
+//    'get  /race/info/:id': 'RaceController.getInfo',
+//    'post /race/racers/add': 'RaceController.addRacers',
+//    'post /race/racers/remove': 'RaceController.removeRacers',
+//    'post /race/update': 'RaceController.update',
+    //'get  /race/delete/:id': 'RaceController.delete',
 
     // Racer
     'post /racer/activate': 'RacerController.activate',
@@ -34,11 +50,12 @@ module.exports.routes = {
     'post /racer/updatePassword': 'RacerController.updatePassword',
     // Reset password
 
-    // Race
-    //'post /race/create': 'RaceController.create',
-    //'get  /race/info/:id': 'RaceController.getInfo',
-    //'post /race/update': 'RaceController.update',
-    //'get  /race/delete/:id': 'RaceController.delete',
+    // Registration
+    //'post /reg/create': 'Registration.create',
+    //'post /reg/replaceRfid': 'Registration.replaceRfid',
+    //'get  /reg/paid': 'Registration: paid,
+    //'get  /reg/unpaid': 'Registration: unpaid,
+    //'get  /reg/recycleRfid': 'Registration: recycleRfid,
 
     // Test page
     'get  /manager/update/:id': 'PageController.managerUpdatePage',
