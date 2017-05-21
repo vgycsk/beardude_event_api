f59db834c90e25e3b7fa0033b2771ebd258e679a
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-05-21 15:15:08 +0800

    Add isRacer test

--- api/policies/isRacer.js
+++ api/policies/isRacer.js
@@ -3,7 +3,7 @@
 
 // Loosly check if logged in
 module.exports = function (req, res, callback) {
-    if (req.session.racerInfo && req.session.RacerInfo.email) {
+    if (req.session.racerInfo && req.session.racerInfo.email) {
         return callback();
     }
     return res.forbidden('Login required');
---
+++ test/api/policies/isRacer.spec.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-magic-numbers */
+/* global afterEach, beforeEach, describe, it, Racer */
+
+var isRacer = require('../../../api/policies/isRacer.js');
+var sinon = require('sinon');
+var assert = require('assert');
+
+describe('policies/isRacer', function() {
+    var sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('should return true if the user is an active racer', function (done) {
+        var req = {
+            session: {
+                racerInfo: {
+                    email: 'info@beardude.com',
+                    isActive: true
+                }
+            }
+        };
+        var actual;
+        var callbackFunc = function () {
+            actual = 'verified';
+        };
+        var res = {
+            forbidden: function (str) {
+                return str;
+            }
+        };
+        var expected = 'verified';
+
+        isRacer(req, res, callbackFunc)
+        assert.equal(actual, expected);
+        done();
+    });
+
+    it('should return Login required if no session data found', function (done) {
+        var req = {
+            session: {}
+        };
+        var actual;
+        var mockData;
+        var callbackFunc = function () {
+            return 'verified';
+        };
+        var res = {
+            forbidden: function (str) {
+                actual = str;
+            }
+        };
+        var expected = 'Login required';
+
+        isRacer(req, res, callbackFunc);
+        assert.equal(actual, expected);
+        done();
+    });
+});
