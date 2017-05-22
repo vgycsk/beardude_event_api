4c0bd17a61cfbd3cfaf47cd39f9fca0f38ba5805
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-05-22 19:38:55 +0800

    Exclude pageController in test

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,2 +1,2 @@
 mkdir -p build/coverage;
-./node_modules/.bin/istanbul cover --include-all-sources -x "*.js" -x "config/**/*.js" -x "tasks/**/*.js" -x "api/responses/*.js" -x "api/models/*.js" -x "assets/js/dependencies/*" --report lcov --report cobertura --dir build/coverage ./node_modules/.bin/_mocha -- -R spec test/bootstrap.test.js test/**/*.spec.js
+./node_modules/.bin/istanbul cover --include-all-sources -x "*.js" -x "config/**/*.js" -x "tasks/**/*.js" -x "api/responses/*.js" -x "api/models/*.js" -x "assets/js/dependencies/*" -x "api/controllers/PageController.js" --report lcov --report cobertura --dir build/coverage ./node_modules/.bin/_mocha -- -R spec test/bootstrap.test.js test/**/*.spec.js
