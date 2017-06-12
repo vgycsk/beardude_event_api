71fbb2e4377fd25e903cbc83062943bfb0af9725
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-06-12 23:49:38 +0800

    Remove unused scripts

--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Recreate public folder
-rm -rf .tmp/public; mkdir .tmp/public;
--- scripts/copyJs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Copy JS files
-cp -r ./assets/js/ .tmp/public/js;
-./node_modules/.bin/webpack --progress;
