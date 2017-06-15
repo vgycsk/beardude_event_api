2f91f6bcf81433c42664b27a8148453786dffc97
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-06-15 19:27:44 +0800

    Update button component; Fix header style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,10 @@
+/*
+  counter: STR
+  onClick: FUNC
+  style: 'regular'(default) / 'disabled', 'bigIcon', 'bigIconCreate', 'list', 'listFt', 'listFtIcon'
+  text: STR
+  url: STR
+*/
 import Button from './presenter'
 
 export default Button
