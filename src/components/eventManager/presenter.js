b90dc9b68f2131504355c986417476756e56d303
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-07-11 12:46:57 +0800

    Add crud group and race in component; Reuse render

--- src/components/eventManager/presenter.js
+++ src/components/eventManager/presenter.js
@@ -14,20 +14,27 @@ const returnDateTime = (timestamp) => {
   const t = new Date(timestamp + 28800000) // taipei diff
   return t.getUTCFullYear() + '-' + ('0' + (t.getUTCMonth() + 1)).slice(-2) + '-' + ('0' + t.getUTCDate()).slice(-2) + 'T' + ('0' + t.getUTCHours()).slice(-2) + ':' + ('0' + t.getUTCMinutes()).slice(-2) //yyyy-mm-ddThh:mm
 }
-const nameInputs = [{ label: '活動名稱（中）', field: 'nameCht'}, { label: '名稱（英）', field: 'name'}, { label: '地點', field: 'location'}]
-const timeInputs = [{ label: '開始時間', field: 'startTime'},{ label: '結束時間', field: 'endTime'}]
-const checkInputs = [{ label: '公開活動', field: 'isPublic'}, { label: '開放隊伍報名', field: 'isTeamRegistrationOpen'}, { label: '開放個人報名', field: 'isRegistrationOpen'}]
+const nameInputs = [{label: '活動名稱（中）', field: 'nameCht'}, {label: '名稱（英）', field: 'name'}, {label: '地點', field: 'location'}]
+const timeInputs = [{label: '開始時間', field: 'startTime'},{label: '結束時間', field: 'endTime'}]
+const checkInputs = [{label: '公開活動', field: 'isPublic'}, {label: '開放隊伍報名', field: 'isTeamRegistrationOpen'}, {label: '開放個人報名', field: 'isRegistrationOpen'}]
+const groupInfo = [{label: '名稱 (中)', field: 'nameCht'}, {label: '名稱 (英)', field: 'name'}, {label: '名額', field: 'racerNumberAllowed', type: 'number'}]
+const raceInfo = [{label: '名稱 (中)', field: 'nameCht'}, {label: '名稱 (英)', field: 'name'}, {label: '名額', field: 'racerNumberAllowed', type: 'number'}, {label: '圈數', field: 'laps', type: 'number'} ]
+
 const renderInfo = (eventInEdit, selectedEvent, onChange, submit) => <section><ul>
   <li>{nameInputs.map((val, i) => <span key={'name-' + i}><label>{val.label}</label><input type='text' value={valueFunc(eventInEdit, selectedEvent,val.field)} onChange={onChange(val.field)} /></span>)}</li>
   <li>{timeInputs.map((val, i) => <span key={'time-' + i}><label>{val.label}</label><input type='datetime-local' value={(eventInEdit && eventInEdit[val.field]) ? eventInEdit[val.field] : returnDateTime(selectedEvent[val.field])} onChange={onChange(val.field)} /></span>)}</li>
   <li>{checkInputs.map((val, i) => <span key={'time-' + i}><label>{val.label}</label><input type='checkbox' checked={valueFunc(eventInEdit, selectedEvent,val.field)} value={valueFunc(eventInEdit, selectedEvent,val.field)} onChange={onChange(val.field)} /></span>)}<span className={css.right}>{ (eventInEdit) ? <Button text='儲存' onClick={submit()} /> : <Button text='儲存' style='disabled' /> }</span>
 </li>
 </ul></section>
-
-const GroupList = ({groups, name, dragStart, dragOver, dragEnd, deleteHandler}) =>
-  <ul>
-    { groups.map((V, I) => <li key={`gp_${I}`} data-id={V.id} data-group={name} draggable='true' onDragStart={dragStart} onDragOver={dragOver} onDragEnd={dragEnd} ><span className={css.itemExpand}>{`${V.name} (?/${V.racerNumberAllowed})`}</span><span className={css.redTxt} onClick={deleteHandler}>delete</span></li>) }
-  </ul>
+const renderList = (inputs, array, selected, inEdit, onChange, onSelect) => <ul>
+  {array.map((V, I) => <li className={selected === I ? css.selected : css.li } key={'li_' + V.id}>{((selected === I) && inEdit)
+    ? <span>{inputs.map((V, I) => <input key={'input_' + I} type={V.type ? V.type : 'text'} placeholder={V.label} value={inEdit[V.field] ? inEdit[V.field] : array[selected][V.field]} onChange={onChange(V.field)}/>)}</span>
+    : <Button style='list' text={V.nameCht ? V.nameCht : V.name} counter={V.registrations.length + '/' + V.racerNumberAllowed} onClick={onSelect(I)}/>
+  }</li>)}
+</ul>
+const renderFt = (inEdit, selected, onSubmit, onCancel, onDelete, onAdd, onEdit) => <span>{inEdit
+              ? <span><Button style='listFt' text='儲存' onClick={onSubmit} /><Button style='listFt' text='取消' onClick={onCancel}/><span className={css.right}><Button style='listFtAlert' text='刪除' onClick={onDelete} /></span></span>
+              : <span><Button style='listFtIcon' text='+' onClick={onAdd} /> {selected !== -1 && <span className={css.right}><Button style='listFt' text='編輯' onClick={onEdit} /></span>}</span> }</span>
 
 export class EventManager extends BaseComponent {
   constructor (props) {
@@ -39,12 +46,13 @@ export class EventManager extends BaseComponent {
       raceSelected: -1,
       racerSelected: -1,
       groupInEdit: undefined,
+      raceInEdit: undefined,
       editPanel: 0,
       showDialogue: 0,
       dialoguePosHandler: null
     }
     this.dispatch = this.props.dispatch
-    this._bind('handleAddGroup', 'handleCancelAddGroup', 'handleEditGroup', 'handleInputEvent', 'handleSubmitEvent', 'handleSubmitGroup', 'editPanelHandler', 'deleteEventHandler', 'dragStartHandler', 'dragOverHandler', 'dragEndHandler', 'dialogueNagitiveHandler')
+    this._bind('handleAddGroup', 'handleAddRace', 'handleCancelAddGroup', 'handleCancelAddRace', 'handleDeleteGroup', 'handleDeleteRace', 'handleEditGroup', 'handleEditRace', 'handleInputEvent', 'handleSubmitEvent', 'handleSubmitGroup', 'handleSubmitRace', 'handleSelectGroup', 'handleSelectRace', 'deleteEventHandler', 'dragStartHandler', 'dragOverHandler', 'dragEndHandler', 'dialogueNagitiveHandler')
   }
   componentDidMount () {
     this.dispatch(actionCreators.getSelectedEvent(this.props.match.params.id || 'new'))
@@ -53,12 +61,38 @@ export class EventManager extends BaseComponent {
     const onSuccess = () => this.setState({groupSelected: this.props.selectedEvent.groups.length - 1, groupInEdit: {event: this.props.selectedEvent.id}})
     this.dispatch(actionCreators.addGroup(onSuccess))
   }
+  handleAddRace () {
+    const group = this.props.selectedEvent.groups[this.state.groupSelected]
+    const onSuccess = () => this.setState({raceSelected: group.races.length - 1, raceInEdit: {group: group.id}})
+    this.dispatch(actionCreators.addRace(this.state.groupSelected, onSuccess))
+  }
   handleCancelAddGroup () {
     const onSuccess = () => this.setState({groupSelected: - 1, groupInEdit: undefined})
     this.dispatch(actionCreators.cancelAddGroup(onSuccess))
   }
+  handleCancelAddRace () {
+    const onSuccess = () => this.setState({raceSelected: - 1, raceInEdit: undefined})
+    this.dispatch(actionCreators.cancelAddRace(this.state.groupSelected, onSuccess))
+  }
+  handleDeleteGroup () {
+    const group = this.props.selectedEvent.groups[this.state.groupSelected]
+    if (group.registrations.length === 0 && group.races.length === 0) {
+      const onSuccess = () => this.setState({groupSelected: - 1, groupInEdit: undefined})
+      this.dispatch(actionCreators.deleteGroup(this.state.groupSelected, group.id, onSuccess))
+    }
+  }
+  handleDeleteRace () {
+    const race = this.props.selectedEvent.groups[this.state.groupSelected].races[this.state.raceSelected]
+    if (race.registrations.length === 0) {
+      const onSuccess = () => this.setState({raceSelected: - 1, raceInEdit: undefined})
+      this.dispatch(actionCreators.deleteRace(this.state.groupSelected, this.state.raceSelected, race.id, onSuccess))
+    }
+  }
   handleEditGroup () {
     this.setState({groupInEdit: { id: this.props.selectedEvent.groups[this.state.groupSelected].id}})
+  }
+  handleEditRace () {
+    this.setState({raceInEdit: { id: this.props.selectedEvent.groups[this.state.groupSelected].races[this.state.raceSelected].id}})
   }
   handleInputEvent (field) { return (e) => {
     let value = (e.target.value === 'true' || e.target.value === 'false') ? (e.target.value === 'true' ? false : true) : e.target.value
@@ -67,8 +101,20 @@ export class EventManager extends BaseComponent {
   handleInputGroup (field) { return (e) => {
     this.setState({ groupInEdit: (this.state.groupInEdit) ? {...this.state.groupInEdit, [field]: e.target.value} : {[field]: e.target.value}})
   }}
-  handleSelect (field, index) { return (e) => {
-    this.setState({[field + 'Selected']: index})
+  handleInputRace (field) { return (e) => {
+    this.setState({ raceInEdit: (this.state.raceInEdit) ? {...this.state.raceInEdit, [field]: e.target.value} : {[field]: e.target.value}})
+  }}
+  handleSelectRace (index) { return (e) => {
+    this.setState({raceSelected: (this.state.raceSelected === index) ? -1 : index})
+  }}
+  handleSelectGroup (index) { return (e) => {
+    if (!this.state.raceInEdit) {
+      const onSuccess = () => this.setState({groupSelected: (this.state.groupSelected === index) ? -1 : index, raceSelected: -1})
+      if (!this.props.selectedEvent.groups[index].races) {
+         return this.dispatch(actionCreators.getGroup(this.props.selectedEvent.groups[index].id, index, onSuccess))
+      }
+      onSuccess()
+    }
   }}
   handleSubmitEvent () {
     const onSuccess = () => this.setState({eventInEdit: undefined})
@@ -77,6 +123,10 @@ export class EventManager extends BaseComponent {
   handleSubmitGroup () {
     const onSuccess = () => this.setState({groupInEdit: undefined})
     this.dispatch(actionCreators.submitGroup(this.state.groupInEdit, this.state.groupSelected, onSuccess))
+  }
+  handleSubmitRace () {
+    const onSuccess = () => this.setState({raceInEdit: undefined})
+    this.dispatch(actionCreators.submitRace(this.state.raceInEdit, this.state.groupSelected, this.state.raceSelected, onSuccess))
   }
   dragStartHandler (E) {
     console.log(`drag start - ${E.currentTarget.dataset.id}`)
@@ -108,13 +158,6 @@ export class EventManager extends BaseComponent {
       })
     }
   }
-  editPanelHandler (idx) {
-    return (E) => {
-      this.setState({
-        editPanel: idx
-      })
-    }
-  }
   deleteEventHandler (E) {
     E.preventDefault()
     console.log(`delete ${E.currentTarget.dataset.name} - ${E.currentTarget.parentNode.dataset.id}`)
@@ -127,46 +170,22 @@ export class EventManager extends BaseComponent {
   }
   render () {
     const { location, selectedEvent } = this.props
-
+    const { eventInEdit, editPanel, groupInEdit, groupSelected, raceInEdit, raceSelected, showDialogue } = this.state
     if (!selectedEvent) { return <div><Header location={location} nav='event' /><div className={css.loading}>Loading...</div></div> }
-    const groups = selectedEvent.groups
-    const { eventInEdit, editPanel, groupSelected, groupInEdit, showDialogue } = this.state
     return (<div>
       <Header location={location} nav='event' />
       <div className={css.mainBody}>
         {renderInfo(eventInEdit, selectedEvent, this.handleInputEvent, this.handleSubmitEvent)}
         <div className={css.managerList}>
-          <div>
-            <h3>組別</h3>
-            <ul>
-              {groups.map((V, I) => <li className={groupSelected === I ? css.selected : css.li } key={'gp_' + I}>{((groupSelected === I) && groupInEdit)
-                ? <span>
-                    <input type='text' placeholder='名稱 (中)' value={groupInEdit.nameCht ? groupInEdit.nameCht : V.nameCht} onChange={this.handleInputGroup('nameCht')}/>
-                    <input type='text' placeholder='名稱 (英)' value={groupInEdit.name ? groupInEdit.name : V.name} onChange={this.handleInputGroup('name')} />
-                    <input type='number' placeholder='名額' value={groupInEdit.racerNumberAllowed ? groupInEdit.racerNumberAllowed : V.racerNumberAllowed} onChange={this.handleInputGroup('racerNumberAllowed')} />
-                  </span>
-                : <Button style='list' text={V.nameCht ? V.nameCht : V.name} counter={'?/' + V.racerNumberAllowed} onClick={this.handleSelect('group', I)}/>
-              }</li>)}
-            </ul>
-            {groupInEdit
-              ? <span><Button style='listFt' text='儲存' onClick={this.handleSubmitGroup} /><span className={css.right}><Button style='listFt' text='取消' onClick={this.handleCancelAddGroup}/></span></span>
-              : <span><Button style='listFtIcon' text='+' onClick={this.handleAddGroup} /> {groupSelected !== -1 && <span className={css.right}><Button style='listFt' text='編輯' onClick={this.handleEditGroup} /></span>}</span> }
+          <div><h3>組別</h3>
+            {renderList(groupInfo, selectedEvent.groups, groupSelected, groupInEdit, this.handleInputGroup, this.handleSelectGroup)}
+            {renderFt(groupInEdit, groupSelected, this.handleSubmitGroup, this.handleCancelAddGroup, this.handleDeleteGroup, this.handleAddGroup, this.handleEditGroup)}
           </div>
-          <div>
-            <h3>組別賽制</h3>
-              { groups.length > 0 ? <GroupList {...{groups, name: 'race', deleteHandler: this.deleteEventHandler, dragStart: this.dragStartHandler, dragOver: this.dragOverHandler, dragEnd: this.dragEndHandler}} /> : null }
-            <div className={css.editPanel}>
-              <div className={classes(!(editPanel === 2) && css.hidden)}>
-                <div>
-                  <input type='text' placeholder='new race name' />
-                </div>
-                <div><span>OK</span><span onClick={this.editPanelHandler(0)} >Cancel</span></div>
-              </div>
-              <span className={classes((editPanel !== 0) && css.hidden)} onClick={this.editPanelHandler(2)}>+</span>
-            </div>
+          <div><h3>組別賽制</h3>
+            {groupSelected !== -1 && selectedEvent.groups[groupSelected].races && renderList(raceInfo, selectedEvent.groups[groupSelected].races, raceSelected, raceInEdit, this.handleInputRace, this.handleSelectRace)}
+            {(groupSelected !== -1) && renderFt(raceInEdit, raceSelected, this.handleSubmitRace, this.handleCancelAddRace, this.handleDeleteRace, this.handleAddRace, this.handleEditRace)}
           </div>
-          <div>
-            <h3>選手</h3>
+          <div><h3>選手</h3>
           </div>
         </div>
       </div>
--- src/ducks/event.js
+++ src/ducks/event.js
@@ -1,10 +1,19 @@
 // types
 const ADD_GROUP = 'event/ADD_GROUP'
+const GET_GROUP = 'event/GET_GROUP'
 const CANCEL_ADD_GROUP = 'event/CANCEL_ADD_GROUP'
+const DELETE_GROUP = 'event/DELETE_GROUP'
+
+const ADD_RACE = 'event/ADD_RACE'
+const GET_RACE = 'event/GET_RACE'
+const CANCEL_ADD_RACE = 'event/CANCEL_ADD_RACE'
+const DELETE_RACE = 'event/DELETE_RACE'
+
 const GET_EVENTS = 'event/GET_EVENTS'
 const GET_SELECTED_EVENT = 'event/GET_SELECTED_EVENT'
 const SUBMIT_EVENT = 'event/SUBMIT_EVENT'
 const SUBMIT_GROUP = 'event/SUBMIT_GROUP'
+const SUBMIT_RACE = 'event/SUBMIT_RACE'
 const EVENT_ERR = 'event/EVENT_ERR'
 
 const returnSubmitObj = (obj) => { return {
@@ -19,9 +28,45 @@ export const actionCreators = {
     dispatch({type: ADD_GROUP})
     return successCallback()
   },
+  addRace: (groupIndex, successCallback) => (dispatch) => {
+    dispatch({type: ADD_RACE, payload: {groupIndex: groupIndex}})
+    return successCallback()
+  },
   cancelAddGroup: (successCallback) => (dispatch) => {
     dispatch({type: CANCEL_ADD_GROUP})
     return successCallback()
+  },
+  cancelAddRace: (groupIndex, successCallback) => (dispatch) => {
+    dispatch({type: CANCEL_ADD_RACE, payload: {groupIndex: groupIndex}})
+    return successCallback()
+  },
+  deleteGroup: (groupIndex, id, successCallback) => async (dispatch) => {
+    try {
+      const response = await fetch('/api/group/delete/' + id, {credentials: 'same-origin'})
+      const res = await response.json()
+      if (response.status === 200) {
+        dispatch({type: DELETE_GROUP, payload: {groupIndex: groupIndex}})
+        return successCallback()
+      }
+      throw res.message
+
+    } catch (e) {
+      dispatch({type: EVENT_ERR, payload: {error: e}})
+    }
+  },
+  deleteRace: (groupIndex, raceIndex, id, successCallback) => async (dispatch) => {
+    try {
+      const response = await fetch('/api/race/delete/' + id, {credentials: 'same-origin'})
+      const res = await response.json()
+      if (response.status === 200) {
+        dispatch({type: DELETE_RACE, payload: {groupIndex: groupIndex, raceIndex: raceIndex}})
+        return successCallback()
+      }
+      throw res.message
+
+    } catch (e) {
+      dispatch({type: EVENT_ERR, payload: {error: e}})
+    }
   },
   getEvents: () => async (dispatch, getState) => {
     try {
@@ -35,6 +80,20 @@ export const actionCreators = {
       dispatch({type: EVENT_ERR, payload: {error: '取得活動內容失敗'}})
     }
   },
+  getGroup: (id, index, successCallback) => async (dispatch) => {
+    try {
+      const response = await fetch( '/api/group/mgmtInfo/' + id, {credentials: 'same-origin'})
+      const res = await response.json()
+      if (response.status === 200) {
+        dispatch({type: GET_GROUP, payload: {...res, index: index}})
+        return successCallback()
+      }
+      throw res.message
+
+    } catch (e) {
+      dispatch({type: EVENT_ERR, payload: {error: e}})
+    }
+  },
   getSelectedEvent: (id) => async (dispatch, getState) => {
     const store = getState().event.selectedEvent
 
@@ -42,7 +101,7 @@ export const actionCreators = {
         return dispatch({type: GET_SELECTED_EVENT, payload: { event: { groups: [] } }})
     }
     try {
-      const response = await fetch('/api/event/info/' + id, {credentials: 'same-origin'})
+      const response = await fetch('/api/event/mgmtInfo/' + id, {credentials: 'same-origin'})
       const res = await response.json()
       if (response.status === 200) {
         return dispatch({type: GET_SELECTED_EVENT, payload: {...res}})
@@ -79,6 +138,20 @@ export const actionCreators = {
     } catch (e) {
       dispatch({type: EVENT_ERR, payload: {error: e}})
     }
+  },
+  submitRace: (obj, groupIndex, raceIndex, successCallback) => async (dispatch) => {
+    try {
+      const response = await fetch((obj.id) ? '/api/race/update' : '/api/race/create', returnSubmitObj(obj))
+      const res = await response.json()
+      if (response.status === 200) {
+        dispatch({type: SUBMIT_RACE, payload: {...res, groupIndex: groupIndex, raceIndex: raceIndex}})
+        return successCallback()
+      }
+      throw res.message
+
+    } catch (e) {
+      dispatch({type: EVENT_ERR, payload: {error: e}})
+    }
   }
 }
 
@@ -96,6 +169,11 @@ export const reducer = (state = initialState, action) => {
       nextState.selectedEvent.groups.push({})
       return nextState
     }
+    case ADD_RACE: {
+      let nextState = {...state}
+      nextState.selectedEvent.groups[payload.groupIndex].races.push({})
+      return nextState
+    }
     case CANCEL_ADD_GROUP: {
       let nextState = {...state}
       if (!nextState.selectedEvent.groups[nextState.selectedEvent.groups.length -1].id) {
@@ -103,8 +181,31 @@ export const reducer = (state = initialState, action) => {
       }
       return nextState
     }
+    case CANCEL_ADD_RACE: {
+      let nextState = {...state}
+      const group = nextState.selectedEvent.groups[payload.groupIndex]
+      if (!group.races[group.races.length - 1].id) {
+        group.races.pop()
+      }
+      return nextState
+    }
+    case DELETE_GROUP: {
+      let nextState = {...state}
+      nextState.selectedEvent.groups.splice(payload.groupIndex, 1)
+      return nextState
+    }
+    case DELETE_RACE: {
+      let nextState = {...state}
+      nextState.selectedEvent.groups[payload.groupIndex].races.splice(payload.raceIndex, 1)
+      return nextState
+    }
     case GET_EVENTS: {
       return {...state, events: payload.events}
+    }
+    case GET_GROUP: {
+      let nextState = {...state}
+      nextState.selectedEvent.groups[payload.index] = payload.group
+      return nextState
     }
     case GET_SELECTED_EVENT: {
       return {...state, selectedEvent: payload.event}
@@ -115,6 +216,13 @@ export const reducer = (state = initialState, action) => {
     case SUBMIT_GROUP: {
       let nextState = {...state}
       nextState.selectedEvent.groups[payload.index] = payload.group
+      return nextState
+    }
+    case SUBMIT_RACE: {
+      let nextState = {...state}
+      console.log('nextState.selectedEvent.groups[payload.groupIndex]: ', nextState.selectedEvent.groups[payload.groupIndex])
+      nextState.selectedEvent.groups[payload.groupIndex].races[payload.raceIndex] = payload.race
+      console.log('nextState.selectedEvent.groups[payload.groupIndex]: after', nextState.selectedEvent.groups[payload.groupIndex])
       return nextState
     }
     case EVENT_ERR: {
