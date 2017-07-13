338c122ac5341219daca652856081e479b2c39c6
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-07-13 20:42:49 +0800

    Beautify dialog component

--- src/components/Dialogue/presenter.js
+++ src/components/Dialogue/presenter.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import css from './style.css'
 
-export const Dialogue = ({content}) => <span>{(content) && <div className={css.box}>
-  <div className={css.wrap}><div className={css.content}>{content}</div></div>
-</div>}</span>
+export const Dialogue = ({content}) => <span>{content &&
+  <div className={css.box}>
+    <div className={css.wrap}>
+      <div className={css.content}>{content}</div>
+    </div>
+  </div>}</span>
