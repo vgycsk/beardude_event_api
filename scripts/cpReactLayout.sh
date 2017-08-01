c0de344b2872dbc4b0c984b0fdc39d44b70c06b6
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-08-01 16:17:01 +0800

    Update build script

--- scripts/cpReactLayout.sh
+++ scripts/cpReactLayout.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Copy JS files
-cp -r .tmp/public/js/console/sharePage.ejs .tmp/views/;
 ./node_modules/.bin/webpack --config ./webpack.config.prod.js --progress;
+cp -r .tmp/public/js/console/sharePage.ejs .tmp/views/;
