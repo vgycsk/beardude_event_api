9c3e2f55ca794be0926707baed37bb3999de1e21
Azai Chan <azai.tw@gmail.com>
2017-08-08 15:38:23 +0800

    Update event controller tests

--- test/api/controllers/EventController.spec.js
+++ test/api/controllers/EventController.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-magic-numbers */
-/* global describe, Event, it, Manager */
+/* global describe, Event, Group, Race, Registration, it */
 
 var eventController = require('../../../api/controllers/EventController.js')
 var sailsMock = require('sails-mock-models')
@@ -14,7 +14,6 @@ describe('/controllers/EventController', function () {
         body: {
           name: 'new event',
           nameCht: '新比賽',
-          assignedRaceNumber: 60,
           startTime: '2017-10-10T08:00:00-08:00',
           endTime: '2017-10-10T16:00:00-08:00',
           lapDistance: 1100,
@@ -28,236 +27,139 @@ describe('/controllers/EventController', function () {
         }
       }
       var res = {
-        ok: function (obj) {
-          actual = obj
-        },
-        badRequest: function (obj) {
-          actual = obj
-        }
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
       }
       var mockData = req.body
       var mockDataManager = {
         id: 1,
-        events: {
-          add: function () {
-            return true
-          }
-        },
-        save: function () {
-          return true
-        }
+        managers: { add: function () { return true } },
+        save: function () { return true }
       }
       var expected = {
-        message: 'Event created',
-        event: req.body
+        event: {
+          name: 'new event',
+          nameCht: '新比賽',
+          startTime: 1507651200000,
+          endTime: 1507680000000,
+          lapDistance: 1100,
+          location: 'Taipei'
+        }
       }
 
       this.timeout(99)
-      mockData.isPublic = false
       sailsMock.mockModel(Event, 'create', mockData)
-      sailsMock.mockModel(Manager, 'findOne', mockDataManager)
+      sailsMock.mockModel(Event, 'findOne', mockDataManager)
       eventController.create(req, res)
       setTimeout(function () {
         expect(actual).to.deep.equal(expected)
         Event.create.restore()
-        Manager.findOne.restore()
+        Event.findOne.restore()
         done()
       }, 30)
     })
   })
-  describe('.getGeneralInfo()', function () {
+  describe('.getEvents()', function () {
+    it('should return events info', function (done) {
+      var actual
+      var req = {}
+      var res = {
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
+      }
+      var mockData = [
+        {
+          id: 1,
+          name: 'new event',
+          nameCht: '新活動'
+        },
+        {
+          id: 2,
+          name: 'new event 2',
+          nameCht: '新活動 2'
+        }
+      ]
+      var expected = { events: mockData }
+
+      this.timeout(99)
+      sailsMock.mockModel(Event, 'find', mockData)
+      eventController.getEvents(req, res)
+      setTimeout(function () {
+        expect(actual).to.deep.equal(expected)
+        Event.find.restore()
+        done()
+      }, 30)
+    })
+  })
+  describe('.getInfo()', function () {
     it('should return event info if found', function (done) {
       var actual
-      var req = {
-        params: {
-          id: '1'
-        }
-      }
+      var req = { params: { id: '1' } }
       var res = {
-        ok: function (obj) {
-          actual = obj
-        },
-        badRequest: function (obj) {
-          actual = obj
-        }
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
       }
       var mockData = {
         id: 1,
         name: 'new event',
         nameCht: '新活動'
       }
-      var expected = mockData
-
+      var mockGroups = [
+        {
+          id: 1,
+          name: 'Group1',
+          nameCht: '組別1'
+        },
+        {
+          id: 2,
+          name: 'Group2',
+          nameCht: '組別2'
+        }
+      ]
+      var mockRaces = [
+        {
+          id: 1,
+          name: 'Race1',
+          nameCht: 'Race1'
+        },
+        {
+          id: 2,
+          name: 'Race2',
+          nameCht: 'Race2'
+        }
+      ]
+      var mockRegs = [
+        {
+          id: 1,
+          name: 'reg1'
+        },
+        {
+          id: 2,
+          name: 'reg'
+        }
+      ]
+      var expected = {
+        event: mockData
+      }
+      mockData.toJSON = function () { return mockData }
+      mockGroups[0].toJSON = function () { return mockGroups[0] }
+      mockGroups[1].toJSON = function () { return mockGroups[1] }
       this.timeout(99)
       sailsMock.mockModel(Event, 'findOne', mockData)
-      eventController.getGeneralInfo(req, res)
+      sailsMock.mockModel(Group, 'find', mockGroups)
+      sailsMock.mockModel(Race, 'find', mockRaces)
+      sailsMock.mockModel(Registration, 'find', mockRegs)
+      eventController.getInfo(req, res)
       setTimeout(function () {
         expect(actual).to.deep.equal(expected)
         Event.findOne.restore()
+        Group.find.restore()
+        Race.find.restore()
+        Registration.find.restore()
         done()
       }, 30)
     })
   })
-    /*
-    describe('.addManagers()', function () {
-        it('should return error if entered manager already exist', function (done) {
-            var actual;
-            var req = {
-                body: {
-                    event: 1,
-                    managers: [1, 2]
-                }
-            };
-            var res = {
-                ok: function (obj) {
-                    actual = obj;
-                },
-                badRequest: function (obj) {
-                    actual = obj;
-                }
-            };
-            var mockData = {
-                id: 1,
-                name: 'new event',
-                nameCht: '新活動',
-                managers: [1, 2]
-            };
-            var expected = 'No managers to add';
-
-            this.timeout(200);
-            sailsMock.mockModel(Event, 'findOne', mockData);
-            eventController.addManagers(req, res);
-            setTimeout(function () {
-                expect(actual.message).to.equal(expected);
-                Event.findOne.restore();
-                done();
-            }, 150);
-        });
-        it('should add unexist manager', function (done) {
-            var actual;
-            var addedManager = [];
-            var req = {
-                body: {
-                    event: 1,
-                    managers: [1, 2, 3]
-                }
-            };
-            var res = {
-                ok: function (obj) {
-                    actual = obj;
-                },
-                badRequest: function (obj) {
-                    actual = obj;
-                }
-            };
-            var mockData = {
-                id: 1,
-                name: 'new event',
-                nameCht: '新活動',
-                managers: [1, 2],
-                save: function () {
-                    return true;
-                }
-            };
-            var expected = {
-                message: 'Managers added to event',
-                event: 1,
-                managers: [3]
-            };
-
-            mockData.managers.add = function (id) {
-                addedManager.push(id);
-            };
-            this.timeout(99);
-            sailsMock.mockModel(Event, 'findOne', mockData);
-            eventController.addManagers(req, res);
-            setTimeout(function () {
-                expect(actual).to.deep.equal(expected);
-                Event.findOne.restore();
-                done();
-            }, 30);
-        });
-    });
-    describe('.removeManagers()', function () {
-        it('should return error if entered manager does not exist', function (done) {
-            var actual;
-            var req = {
-                body: {
-                    event: 1,
-                    managers: [1, 2]
-                }
-            };
-            var res = {
-                ok: function (obj) {
-                    actual = obj;
-                },
-                badRequest: function (obj) {
-                    actual = obj;
-                }
-            };
-            var mockData = {
-                id: 1,
-                name: 'new event',
-                nameCht: '新活動',
-                managers: [3, 4]
-            };
-            var expected = 'No managers to remove';
-
-            this.timeout(99);
-            sailsMock.mockModel(Event, 'findOne', mockData);
-            eventController.removeManagers(req, res);
-            setTimeout(function () {
-                expect(actual.message).to.equal(expected);
-                Event.findOne.restore();
-                done();
-            }, 30);
-        });
-        it('should remove manager from event', function (done) {
-            var actual;
-            var removedManager = [];
-            var req = {
-                body: {
-                    event: 1,
-                    managers: [1, 2, 3]
-                }
-            };
-            var res = {
-                ok: function (obj) {
-                    actual = obj;
-                },
-                badRequest: function (obj) {
-                    actual = obj;
-                }
-            };
-            var mockData = {
-                id: 1,
-                name: 'new event',
-                nameCht: '新活動',
-                managers: [1, 2, 3, 4, 5],
-                save: function () {
-                    return true;
-                }
-            };
-            var expected = {
-                message: 'Managers removed from event',
-                event: 1,
-                managers: [1, 2, 3]
-            };
-
-            mockData.managers.remove = function (id) {
-                removedManager.push(id);
-            };
-            this.timeout(99);
-            sailsMock.mockModel(Event, 'findOne', mockData);
-            eventController.removeManagers(req, res);
-            setTimeout(function () {
-                expect(actual).to.deep.equal(expected);
-                Event.findOne.restore();
-                done();
-            }, 30);
-        });
-    });
-    */
   describe('.update()', function () {
     it('should update specified fields', function (done) {
       var actual
@@ -268,12 +170,8 @@ describe('/controllers/EventController', function () {
         }
       }
       var res = {
-        ok: function (obj) {
-          actual = obj
-        },
-        badRequest: function (obj) {
-          actual = obj
-        }
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
       }
       var mockData = req.body
       var mockDataUpdate = [{
@@ -281,10 +179,7 @@ describe('/controllers/EventController', function () {
         name: 'new event1',
         assignedRaceNumber: 50
       }]
-      var expected = {
-        message: 'Event updated',
-        event: mockDataUpdate[0]
-      }
+      var expected = { event: mockDataUpdate[0] }
 
       mockData.id = 1
       this.timeout(99)
@@ -300,116 +195,110 @@ describe('/controllers/EventController', function () {
       }, 30)
     })
   })
-  describe('.updateSwitch()', function () {
-    it('should update specified fields', function (done) {
+  describe('.delete()', function () {
+    it('should return error if is a public event', function (done) {
       var actual
-      var req = {
-        body: {
-          event: 1,
-          isRegistrationOpen: true,
-          isTeamRegistrationOpen: true,
-          isPublic: true
-        }
-      }
+      var req = { params: { id: 1 } }
       var res = {
-        ok: function (obj) {
-          actual = obj
-        },
-        badRequest: function (obj) {
-          actual = obj
-        }
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
       }
-      var mockDataUpdate = [{
+      var mockData = {
         id: 1,
         name: 'new event1',
-        isRegistrationOpen: true,
-        isTeamRegistrationOpen: true,
         isPublic: true
-      }]
-      var expected = {
-        message: 'Event boolean field(s) updated',
-        event: mockDataUpdate[0]
       }
 
       this.timeout(99)
-      sailsMock.mockModel(Event, 'update', mockDataUpdate)
-
-      eventController.updateSwitch(req, res)
+      sailsMock.mockModel(Event, 'findOne', mockData)
+      eventController.delete(req, res)
       setTimeout(function () {
-        expect(actual).to.deep.equal(expected)
-        Event.update.restore()
-        done()
-      }, 30)
-    })
-  })
-  describe('.assignTesterRfid()', function () {
-    it('should return error if RFID already assigned', function (done) {
-      var actual
-      var req = {
-        body: {
-          event: 1,
-          epc: 'abc0001'
-        }
-      }
-      var res = {
-        ok: function (obj) {
-          actual = obj
-        },
-        badRequest: function (obj) {
-          actual = obj
-        }
-      }
-      var mock = {
-        id: 1,
-        name: 'new event1',
-        testerEpc: ['abc0000', 'abc0001', 'abc0002', 'abc0003']
-      }
-      var expected = 'Tester RFID already assigned'
-
-      this.timeout(50)
-      sailsMock.mockModel(Event, 'findOne', mock)
-      eventController.assignTesterRfid(req, res)
-      setTimeout(function () {
-        expect(actual.message).to.equal(expected)
+        expect(actual.message).to.equal('Cannot delete a public event')
         Event.findOne.restore()
         done()
       }, 30)
     })
-    it('should assign tester RFID', function (done) {
+
+    it('should return error if is an ongoing event', function (done) {
       var actual
-      var req = {
-        body: {
-          event: 1,
-          epc: 'abc0007'
-        }
-      }
+      var req = { params: { id: 1 } }
       var res = {
-        ok: function (obj) {
-          actual = obj
-        },
-        badRequest: function (obj) {
-          actual = obj
-        }
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
       }
-      var mock = {
+      var mockData = {
         id: 1,
         name: 'new event1',
-        testerEpc: ['abc0000', 'abc0001', 'abc0002', 'abc0003']
-      }
-      var expected = {
-        message: 'Tester registered',
-        event: 1,
-        epc: 'abc0007'
+        isPublic: false,
+        startTime: Date.now() - 10000,
+        endTime: Date.now() + 10000
       }
 
-      this.timeout(50)
-      sailsMock.mockModel(Event, 'findOne', mock)
-      sailsMock.mockModel(Event, 'update')
-      eventController.assignTesterRfid(req, res)
+      this.timeout(99)
+      sailsMock.mockModel(Event, 'findOne', mockData)
+
+      eventController.delete(req, res)
       setTimeout(function () {
-        expect(actual).to.deep.equal(expected)
+        expect(actual.message).to.equal('Cannot delete an ongoing event')
         Event.findOne.restore()
-        Event.update.restore()
+        done()
+      }, 30)
+    })
+    it('should return error if event contains groups', function (done) {
+      var actual
+      var req = { params: { id: 1 } }
+      var res = {
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
+      }
+      var mockData = {
+        id: 1,
+        name: 'new event1',
+        isPublic: false,
+        startTime: 1507651200000,
+        endTime: 1507680000000,
+        groups: [
+          {
+            id: 1,
+            name: 'group1'
+          }
+        ]
+      }
+
+      this.timeout(99)
+      sailsMock.mockModel(Event, 'findOne', mockData)
+
+      eventController.delete(req, res)
+      setTimeout(function () {
+        expect(actual.message).to.equal('Cannot delete an event that contains group')
+        Event.findOne.restore()
+        done()
+      }, 30)
+    })
+    it('should delete event', function (done) {
+      var actual
+      var req = { params: { id: 1 } }
+      var res = {
+        ok: function (obj) { actual = obj },
+        badRequest: function (obj) { actual = obj }
+      }
+      var mockData = {
+        id: 1,
+        name: 'new event1',
+        isPublic: false,
+        startTime: 1507651200000,
+        endTime: 1507680000000,
+        groups: []
+      }
+
+      this.timeout(99)
+      sailsMock.mockModel(Event, 'findOne', mockData)
+      sailsMock.mockModel(Event, 'destroy', { id: 1 })
+      eventController.delete(req, res)
+      setTimeout(function () {
+        expect(actual).to.deep.equal({ event: 1 })
+        Event.findOne.restore()
+        Event.destroy.restore()
         done()
       }, 30)
     })
