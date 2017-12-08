ca44f49c2f0f26751d3531c2b0bb12b949c865ea
Azai Chan <azai.tw@gmail.com>
2017-12-08 13:17:12 +0800

    Move race logic to api server

--- api/services/dataService.js
+++ api/services/dataService.js
@@ -40,6 +40,7 @@ var dataService = {
     })
     return updateObj
   },
+  /*
   returnUpdatedRaceNotes: function (raceId, raceNote, existingRaceNotes) {
     var raceNotes = existingRaceNotes
     var dataExist
@@ -55,6 +56,8 @@ var dataService = {
     if (!dataExist) { raceNotes.push({ race: raceId, note: raceNote }) }
     return raceNotes
   },
+  */
+  // Validate if tag entry meets interval requirement
   isValidReadTagInterval: function (epc, timestamp, recordsHashTable, intervalInMs) {
     var records = recordsHashTable[epc]
     var lastRecord
@@ -81,6 +84,12 @@ var dataService = {
     getCode(code)
     return q.promise
   },
+  // 把timestamp parse成日期時間
+  returnFormattedTime: (milS) => {
+    const sec = ((milS % 60000) / 1000).toFixed(2)
+    const min = Math.floor(milS / 60000)
+    return min + ':' + (sec < 10 ? '0' : '') + sec
+  },
   returnRacesByOrder: function (races, order) {
     let result = []
     order.map(raceId => { races.map(race => { if (race.id === raceId) { result.push(race) } }) })
@@ -89,6 +98,82 @@ var dataService = {
   // 1. lowercase 2. remove special char 3. condense
   sluggify: function (string) {
     return string.toLowerCase().replace(/[^\w\s]/gi, '').replace(/ +/g, '')
+  },
+  // 回傳可晉級的比賽ID
+  returnAdvanceToId: (rank, advancingRules) => {
+    for (var i = 0; i < advancingRules.length; i += 1) {
+      if (rank >= advancingRules[i].rankFrom && rank <= advancingRules[i].rankTo) { return advancingRules[i].toRace }
+    }
+    return undefined
+  },
+  // 讀取並回傳所有單圈成績
+  // 完成圈數顯示單圈成績, 進行中顯示時鐘emoji, 沒成績顯示-
+  returnLapRecord: (result, laps, startTime, raceStatus) => {
+    let output = []
+    let lastRecord = startTime
+    let lapsLeft = laps
+    let i
+
+    if (result.length > 0) {
+      for (i = 1; i <= result.length; i += 1) {
+        if (result[i]) {
+          output.push(dataService.returnFormattedTime(result[i] - lastRecord))
+          lastRecord = result[i]
+          lapsLeft -= 1
+        } else if (lapsLeft > 0 && raceStatus === 'started') {
+          output.push('🕒')
+          lapsLeft -= 1
+        }
+      }
+    }
+    for (i = 0; i < lapsLeft; i += 1) { output.push('-') }
+    return output
+  },
+  returnSortedResult: (race, regs) => {
+    let sortTable = []
+    let incomplete = []
+    let notStarted = []
+    const lastRecordIndex = race.laps
+
+    race.registrationIds.map(regId => {
+      const reg = regs.filter(V => (V.id === regId))[0]
+      if (reg) {
+        const record = race.recordsHashTable[reg.epc]
+        let obj = { epc: reg.epc, registration: reg.id, raceNumber: reg.raceNumber, lapsCompleted: 0, record: [] }
+        if (record) { // has epc record
+          obj.lapsCompleted = record.length - 1
+          obj.record = record
+          if (record[lastRecordIndex]) { // 1. completed race
+            obj.lastValidRecord = record[lastRecordIndex]
+            sortTable.push(obj)
+          } else { // 2. not complete
+            obj.lastValidRecord = record[record.length - 1]
+            incomplete.push(obj)
+          }
+        } else { // 3. no epc record
+          notStarted.push(obj)
+        }
+      }
+    })
+    sortTable.sort((a, b) => a.lastValidRecord - b.lastValidRecord) // sort completed racer by last lap record
+    incomplete.sort((a, b) => b.lapsCompleted - a.lapsCompleted) // sort incompleted by laps
+    incomplete.sort((a, b) => (a.lapsCompleted === b.lapsCompleted) ? a.lastValidRecord - b.lastValidRecord : 0) // sort incompleted same-lap by time
+    notStarted.sort((a, b) => a.raceNumber - b.raceNumber) // sort notStart by raceNumber
+    sortTable = sortTable.concat(incomplete).concat(notStarted)
+    // output: { epc, id, raceNumber, lastValidRecord, lapsCompleted, record }
+    return sortTable
+  },
+  // 將 race.recordsHashTable parse成race.result. 並依成績做排序
+  returnRaceResult: (race, regs) => {
+    const sortTable = dataService.returnSortedResult(race, regs)
+    return sortTable.map((item, index) => ({
+      epc: item.epc,
+      registration: item.registration,
+      sum: (item.lastValidRecord) ? dataService.returnFormattedTime(item.lastValidRecord - race.startTime) : '-',
+      laps: item.lapsCompleted,
+      lapRecords: dataService.returnLapRecord(item.record, race.laps, race.startTime, race.raceStatus),
+      advanceTo: dataService.returnAdvanceToId(index, race.advancingRules)
+    }))
   },
   // raceObj: { event: ID, (pacerEpcSlave: STR, pacerEpc: STR) }
   returnSlaveEpcMap: function (raceObj) {
@@ -108,14 +193,18 @@ var dataService = {
     newRecordsToAdd.map(function (record) {
       var epc = record.epc
       var isSlave
+      // 1. check if this is a slave tag
       if (typeof slaveEpcMap[record.epc] !== 'undefined') {
         epc = slaveEpcMap[record.epc]
         isSlave = true
       }
-      if (!result.recordsHashTable[epc]) { result.recordsHashTable[epc] = [] } // 還沒資料的話, 新增一個空的array
+      // 2. Initialize record if not yet available
+      if (!result.recordsHashTable[epc]) { result.recordsHashTable[epc] = [] }
+      // 3. Validate interval
       if (dataService.isValidReadTagInterval(epc, record.timestamp, result.recordsHashTable, validIntervalMs)) {
         result.recordsHashTable[epc].push(record.timestamp)
-        if (isSlave) {  // Save slave epc read stat, for debugging
+        // 4. If slave tag, save read index to slave stats
+        if (isSlave) {
           if (typeof result.slaveEpcStat[epc] === 'undefined') { result.slaveEpcStat[epc] = [] }
           result.slaveEpcStat[epc].push(result.recordsHashTable[epc].length - 1)
         }
