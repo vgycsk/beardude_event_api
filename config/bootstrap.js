afb1ffb310bba858e69676b447f8a4ee4d67c198
Azai Chan <azai.tw@gmail.com>
2018-01-13 14:18:23 +0800

    Correct init manager account

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -4,17 +4,16 @@ module.exports.bootstrap = function(cb) {
     // Initiate first manager account
     Manager.find({})
     .then(function (managerData) {
-        if (managerData.length === 0) {
-          return Manager.create({
-            email: 'azaitw@github.com',
-            phone: '12345678',
-            firstName: 'Azai',
-            lastName: 'Chan',
-            password: '123',
-            isActive: true
-          })
-        }
-        return false;
+      if (managerData.length === 0) {
+        return Manager.create({
+          email: 'azaitw@github.com',
+          phone: '12345678',
+          firstName: 'Azai',
+          lastName: 'Chan',
+          password: '123'
+        })
+      }
+      return false;
     })
     .then(function () {
       return System.count({})
